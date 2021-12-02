4dbd0cbd26516b929d7e03ff3af6dbf149aee3cb
SWH <sungwoo166@gmail.com>
2021-12-02 20:35:06 +0900

    모델 추가
    .

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Customer(models.Model):
+    #OneToOneField ==> user는 하나의 customer만 가질수 있다.  on_delete CASCADE는 user삭제시 그 유저와 관련된거 다 삭제
+    user = models.OneToOneField(User,on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=True)
+    #image
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    #customer는 많은 order를 가질수 있기 때문에 ForeignKey 즉, Customer의 자식, 또한 연결
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.product.name)
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
