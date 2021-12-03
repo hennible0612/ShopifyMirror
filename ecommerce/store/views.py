cda5aa30adbef8721e4c6c8cd81d7c320bc2d148
SWH <sungwoo166@gmail.com>
2021-12-03 16:17:47 +0900

    checkout, cart dynamic하게 바꿈
    .

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,10 +7,25 @@ def store(request):
     return render(request,'store/store.html',context)
 
 def cart(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created=Order.objects.get_or_create(customer=customer, complete=False) #현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
+        items = order.orderitem_set.all() #order에 있는 모든 items 가져오기
+    else:
+        item=[]
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context ={'items':items, 'order':order}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
-    context ={}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,
+                                                     complete=False)  # 현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
+        items = order.orderitem_set.all()  # order에 있는 모든 items 가져오기
+    else:
+        item = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request,'store/checkout.html',context)
     
