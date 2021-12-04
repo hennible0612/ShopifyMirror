b384cff1ea76cc81105829bfff67f9e3e2f7ad4e
SWH <sungwoo166@gmail.com>
2021-12-04 14:58:31 +0900

    add to cart 자바스크립트 수정
    .

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -18,8 +18,9 @@ def cart(request):
         order, created=Order.objects.get_or_create(customer=customer, complete=False) #현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
         items = order.orderitem_set.all() #OrderItem은 Order의 자식이기 현재Order의 OrderItem을 소문자로 가져올수 있음
     else:
-        item=[]
+        items=[]
         order = {'get_cart_total':0, 'get_cart_items':0}
+
     context ={'items':items, 'order':order}
     return render(request,'store/cart.html',context)
 
@@ -30,7 +31,7 @@ def checkout(request):
                                                      complete=False)  # 현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
         items = order.orderitem_set.all()  # order에 있는 모든 items 가져오기
     else:
-        item = []
+        items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {'items': items, 'order': order}
     return render(request,'store/checkout.html',context)
