57608bf7be334366fd9ad6272002476e3710941f
SWH <sungwoo166@gmail.com>
2021-12-06 11:48:51 +0900

    아이템 카트에 추가
    .

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 from django.http import JsonResponse
+import json
+
 """
 메인 페이지
 모든 제품 표시
@@ -38,4 +40,27 @@ def checkout(request):
 
 
 def updateItem(request):#아이템 추가할때마다 제이슨 리스폰스 보냄
+    data = json.loads(request.body) #json객체를 body를 data에저장 받고 data에 저장
+    productId= data['productId'] #제이슨은 dict 형태이니 요렇게 접속 가능
+    action = data['action']
+    print('ProductId:', productId)
+    print('Action:', action)
+
+    customer = request.user.customer #현재 Customer 가져옴
+    product = Product.objects.get(id=productId) #ID사용해서 현재 클릭한 product 저장
+    order, created = Order.objects.get_or_create(customer=customer, #현재 customer의 order를 get or create
+                                                 complete=False)  # 현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
+    orderItem, created = OrderItem.objects.get_or_create(order=order, #주문 아이템들
+                                                         product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added!', safe=False)
