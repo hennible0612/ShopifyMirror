3a66045c6cefae37f16940aa078f8c8ef1db6e75
SWH <sungwoo166@gmail.com>
2021-12-07 17:55:27 +0900

    쿠키랑 쿠키카트 추가
    .

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -37,9 +37,40 @@ def cart(request):
         cartItems = order.get_cart_items
 
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])  # 스트링으로 된거 풀고 cart에저장
+        except:
+            cart = {}
+        print('Cart:',cart)
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
+        for i in cart:        # 카트에 들어있는 quantity확인인
+            try:
+                cartItems += cart[i]['quantity']
+
+                product = Product.objects.get(id=i) #아이디 기준으로 product에 저장
+                total = (product.price * cart[i]["quantity"]) #total 비용계산
+
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]["quantity"]
+
+                item = {
+                    'product':{
+                        'id':product.id,
+                        'name':product.name,
+                        'price':product.price,
+                        'imageURL':product.imageURL,
+                    },
+                    'quantity':cart[i]["quantity"],
+                    'get_total':total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/cart.html', context)
@@ -48,15 +79,15 @@ def cart(request):
 def checkout(request):
     if request.user.is_authenticated:
         customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer,
-                                                     complete=False)  # 현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)  # 현재 가져온 customer의 order가 없으면 만들고 있으면 가져오기,
         items = order.orderitem_set.all()  # order에 있는 모든 items 가져오기
         cartItems = order.get_cart_items
 
     else:
-        items = []
+        items= []
         order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
         cartItems = order['get_cart_items']
+
 
     context = {'items': items, 'order': order, 'cartItems': cartItems}
     return render(request, 'store/checkout.html', context)
